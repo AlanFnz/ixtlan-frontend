25adcb78a45cc2a5b0724792873605562d0a64f3
AlanFnzCodea <alan.fernandez@codeait.com>
2022-08-28 19:37:00 -0300

    initial commit

---
+++ apps/ixtlan-web/pages/index.tsx
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export function Index() {
+  return (
+    <StyledPage>
+      <span>Ixtlan</span>
+    </StyledPage>
+  );
+}
+
+const StyledPage = styled.div`
+  .page {
+  }
+`;
+
+export default Index;
