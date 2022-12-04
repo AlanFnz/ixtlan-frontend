3662999ddc8f8441c141505e933b74fb067f9a86
AlanFnzCodea <alan.fernandez@codeait.com>
2022-12-04 16:25:22 -0300

    added sections

---
+++ apps/ixtlan-web/pages/contact/contact.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Contact = () => {
+  return <Container></Container>;
+};
+
+const Container = styled.div`
+  width: 100vw;
+`;
+
+export default Contact;
